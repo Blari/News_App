dbf021beda47c19d4fd929f250dabcaceab190e2
Blari <Blarior@gmail.com>
2019-11-08 15:24:49 +0300

    main files

---
+++ app.js
@@ -0,0 +1,6 @@
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+xhr.addEventListener("load", () => {
+    console.log(xhr.responseText);
+});
+xhr.send();
